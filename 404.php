e10c3007f163474378ddc8f5824f1163440c12fd
mstpierre73 <naranjaqc@hotmail.com>
2019-08-14 20:43:13 -0400

    add custom pages to template

--- 404.php
+++ 404.php
@@ -12,6 +12,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="wrapper">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
@@ -53,6 +54,7 @@ get_header();
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
+			</div><!-- .wrapper -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
